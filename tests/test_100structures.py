05402d95aa0a7be18fdc93c508d8bea96ceb41f8
Vojtech Spiwok <spiwokv@vscht.cz>
2018-05-14 21:34:59 +0200

    Update test_100structures.py

--- tests/test_100structures.py
+++ tests/test_100structures.py
@@ -5,12 +5,15 @@ import keras as krs
 import argparse as arg
 import datetime as dt
 import sys
+import os
 
 import encodetrajlib
 
 def test_100_structures():
-  ae, cor = encodetrajlib.encodetrajectory(infilename='./traj_fit_small.xtc',
-                                           intopname='./reference.pdb',
+  myinfilename = os.path.join(os.path.dirname(__file__), 'traj_fit_small.xtc')
+  myintopname = os.path.join(os.path.dirname(__file__), 'reference.pdb')
+  ae, cor = encodetrajlib.encodetrajectory(infilename=myinfilename,
+                                           intopname=myintopname,
                                            boxx=1, boxy=1, boxz=1, epochs=2000)
   assert(cor > 0.95)
 
